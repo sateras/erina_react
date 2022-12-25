a7e517d9a67a4e534664eb6cf8796095ffb2f3b0
sateras <satpekov02@mail.ru>
2022-12-25 20:17:35 +0600

    Add components

--- src/App.js
+++ src/App.js
@@ -2,64 +2,18 @@ import "./assets/styles/App.scss";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 
 function App() {
   return (
     <div>
-      <div style={{ display: "none" }} className="overlay">
-        <div className="drawer">
-          <h3>CART</h3>
-          <div className="cart">
-            <div className="cartItem">
-              <div className="cartItemImg">
-                <img width={75} src="/products/1.jpg" alt="prod" />
-              </div>
-              <div>
-                <p>Ladies yellow top</p>
-                <b>$25</b>
-              </div>
-              <button className="cart_btn">Delete</button>
-            </div>
-            <div className="cartItem">
-              <div className="cartItemImg">
-                <img width={75} src="/products/1.jpg" alt="prod" />
-              </div>
-              <div>
-                <p>Ladies yellow top</p>
-                <b>$25</b>
-              </div>
-              <button className="cart_btn">Delete</button>
-            </div>
-            <div className="cartItem">
-              <div className="cartItemImg">
-                <img width={75} src="/products/1.jpg" alt="prod" />
-              </div>
-              <div>
-                <p>Ladies yellow top</p>
-                <b>$25</b>
-              </div>
-              <button className="cart_btn">Delete</button>
-            </div>
-          </div>
-          <div className="cartTotalBlock">
-            <div className="cartTotal">
-              <span>Total</span>
-              <div></div>
-              <b>$255</b>
-            </div>
-            <div>
-              <span>Nalog 5%</span>
-              <div></div>
-              <b>$12.25</b>
-            </div>
-          </div>
-          <button className="greenBtn">Confirm</button>
-        </div>
-      </div>
+      <Drawer />
       <Header />
+
       <div className="content">
         <div className="container">Content is here!</div>
       </div>
+
       <main className="main container">
         <div className="main_fixed">
           <div>
@@ -69,19 +23,16 @@ function App() {
           </div>
           <div>
             <img src="/svg/card_icon.svg" alt="icon" />
-
             <h6>Secure Payments</h6>
             <p>Eonim ad minim veniam liquip tomader</p>
           </div>
           <div>
             <img src="/svg/return_icon.svg" alt="icon" />
-
             <h6>Easy Returns</h6>
             <p>Be enim ad minim veniam liquipa ami tomader</p>
           </div>
           <div>
             <img src="/svg/time_icon.svg" alt="icon" />
-
             <h6>24/7 Support</h6>
             <p>Ut enim ad minim veniam liquip ami tomader</p>
           </div>
@@ -89,14 +40,15 @@ function App() {
         <div>
           <h3>Latest arrival</h3>
           <h4>Showing our latest arrival on this summer</h4>
+
           <div className="items_table">
+            <Card />
             <Card />
             <Card />
             <Card />
           </div>
         </div>
       </main>
-
       <Footer />
     </div>
   );
---
+++ src/components/Drawer.js
@@ -0,0 +1,56 @@
+function Drawer(params) {
+  return (
+    <div style={{ display: "none" }} className="overlay">
+      <div className="drawer">
+        <h3>CART</h3>
+        <div className="cart">
+          <div className="cartItem">
+            <div className="cartItemImg">
+              <img width={75} src="/products/1.jpg" alt="prod" />
+            </div>
+            <div>
+              <p>Ladies yellow top</p>
+              <b>$25</b>
+            </div>
+            <button className="cart_btn">Delete</button>
+          </div>
+          <div className="cartItem">
+            <div className="cartItemImg">
+              <img width={75} src="/products/1.jpg" alt="prod" />
+            </div>
+            <div>
+              <p>Ladies yellow top</p>
+              <b>$25</b>
+            </div>
+            <button className="cart_btn">Delete</button>
+          </div>
+          <div className="cartItem">
+            <div className="cartItemImg">
+              <img width={75} src="/products/1.jpg" alt="prod" />
+            </div>
+            <div>
+              <p>Ladies yellow top</p>
+              <b>$25</b>
+            </div>
+            <button className="cart_btn">Delete</button>
+          </div>
+        </div>
+        <div className="cartTotalBlock">
+          <div className="cartTotal">
+            <span>Total</span>
+            <div></div>
+            <b>$255</b>
+          </div>
+          <div>
+            <span>Nalog 5%</span>
+            <div></div>
+            <b>$12.25</b>
+          </div>
+        </div>
+        <button className="greenBtn">Confirm</button>
+      </div>
+    </div>
+  );
+}
+
+export default Drawer;
